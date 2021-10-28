a2ee4d60dd57b6dcfd0be2e5d663cb37be573eae
Francisco Rocha <fran_25_02@hotmail.com>
2021-10-28 18:59:36 -0300

    Primer commit

---
+++ Salvo/Salvo/Models/SalvoContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Salvo.Models
+{
+    public class SalvoContext : DbContext 
+    {
+        public SalvoContext(DbContextOptions<SalvoContext>options) : base(options)
+        {
+
+        }
+
+        //public DbSet<demo> demos { set; get; }
+    }
+
+    /*
+    public class demo
+    {
+        public int id;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+    }
+    */
+
+}
